8c2702b847976a7de74b9e81c19cf4abb5457162
malcolmdzimati <malcolmdzimati@gmail.com>
2025-05-10 12:37:51 +0200

    chore: adding health checks

--- PropertyToolsByRotaBackend/cmd/main.go
+++ PropertyToolsByRotaBackend/cmd/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	// other routes...
+	router.Run(":8080")
 }
