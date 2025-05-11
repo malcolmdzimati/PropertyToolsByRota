dd368d06a411a2fbacc9319c9d851986291cbe7a
malcolmdzimati <malcolmdzimati@gmail.com>
2025-05-11 19:17:07 +0200

    chore: removing unneedy

--- PropertyToolsByRotaBackend/cmd/main.go
+++ PropertyToolsByRotaBackend/cmd/main.go
@@ -3,15 +3,9 @@ package main
 import (
 	"PropertyToolsByRotaBackend/internal/router"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"log"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
 	r := gin.Default()
 
 	// Set up routes
